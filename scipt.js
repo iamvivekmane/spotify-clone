8d55e9f3b6e92e18bfab282ed7b1745b8839dc0c
Vivek Mane <vivekmane172@gmail.com>
2025-06-20 22:11:41 +0530

    Added getsongs() function

--- scipt.js
+++ scipt.js
@@ -1,1 +1,29 @@
-console.log('Hello');
+console.log("Hello");
+
+async function getSongs() {
+  let a = await fetch("http://172.20.10.2:3000/songs/");
+  let response = await a.text();
+  console.log(response);
+
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href);
+    }
+  }
+  return songs;
+} 
+async function main() {
+  //Returns the list of all the songs
+  let songs = await getSongs();
+
+  //Play songs
+  var audio = new Audio(songs[0]);
+  audio.play();
+}
+
+main();
