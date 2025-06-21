173f6b8d49b74fc43199f4703541de87ae229111
Vivek Mane <vivekmane172@gmail.com>
2025-06-21 12:31:46 +0530

    Added playlist design and fetch() function

--- scipt.js
+++ scipt.js
@@ -12,18 +12,41 @@ async function getSongs() {
   for (let index = 0; index < as.length; index++) {
     const element = as[index];
     if (element.href.endsWith(".mp3")) {
-      songs.push(element.href);
+      songs.push(element.href.split("/songs/")[1]);
     }
   }
   return songs;
-} 
+}
 async function main() {
   //Returns the list of all the songs
   let songs = await getSongs();
 
+  let songUl = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songUl.innerHTML =
+      songUl.innerHTML +
+      `<li>
+                <img class="invert" src="music.svg" alt="" />
+                <div class="info">
+                  <div>${song.replaceAll("%20", " ")}</div>
+                  <div>Artist</div>
+                </div>
+                <div class="playnow">
+                  <span>Play Now</span>
+                  <img class="invert" src="play.svg" alt="" />
+                </div>
+              </li>`;
+  }
   //Play songs
   var audio = new Audio(songs[0]);
-  audio.play();
+  //   audio.play();
+
+  audio.addEventListener("loadeddata", () => {
+    //Duration variable holds the duration of the audio file in seconds
+    console.log(audio.duration, audio.currentSrc, audio.currentTime);
+  });
 }
 
 main();
